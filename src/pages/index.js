7a0abfff57aeb6d81137ffd41af99ec52312bc2e
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-04-01 16:49:51 +0530

    setting up projects folders

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export {default as PageNotFound} from "./404/PageNotFound";
+export {default as Details} from "./details/Details";
+export {default as Explore} from "./explore/Explore";
+export {default as Home} from "./home/Home"; // Fix: Changed file name to match existing file name
+export {default as SearchResult} from "./searchResult/SearchResult";
