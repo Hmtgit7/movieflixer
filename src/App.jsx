afe14b67baf0787845e28ea1d306a514bd1ac27a
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-04-01 16:48:16 +0530

    implementing routes and api configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
 import { useEffect } from "react";
-import "./App.css";
-import {getMovies} from "./utils/api"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+
+import { Header, Footer } from "./components";
+import { Home, Details, SearchResult, Explore, PageNotFound } from "./pages";
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+
+  useEffect(() => {
+    fetchApiConfig();
+  }, []);
+
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log(res);
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+
+      dispatch(getApiConfiguration(url));
+    });
+  };
+
   return (
-    <>
-      <h2>app</h2>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
