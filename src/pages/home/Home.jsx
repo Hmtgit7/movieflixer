263a1409112f58a6653d90b7aff66e4d50037de6
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-04-03 09:05:20 +0530

    changes on formatting

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import "./Home.scss";
+
 import HeroBanner from "./heroBanner/HeroBanner";
 
 const Home = () => {
-  return <div className="homePage">
-    <HeroBanner />
-  </div>;
+  return (
+    <div className="homePage">
+      <HeroBanner />
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-export {default as PageNotFound} from "./404/PageNotFound";
-export {default as Details} from "./details/Details";
-export {default as Explore} from "./explore/Explore";
-export {default as Home} from "./home/Home"; // Fix: Changed file name to match existing file name
-export {default as SearchResult} from "./searchResult/SearchResult";
+export { default as PageNotFound } from "./404/PageNotFound";
+export { default as Details } from "./details/Details";
+export { default as Explore } from "./explore/Explore";
+export { default as Home } from "./home/Home";
+export { default as SearchResult } from "./searchResult/SearchResult";
