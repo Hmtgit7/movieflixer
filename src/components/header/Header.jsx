724eefcaec1336f75ca9ed3f4d213a3ed15b7c24
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-04-04 19:39:47 +0530

    Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import "./Header.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
 
-export default Header
+  const navigationHandler=(path)=>{
+    navigate(path);
+  }
+
+  return (
+    <header className="header">
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="movie-flixer" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItems">Movies</li>
+          <li className="menuItems">TV Shows</li>
+          <li className="menuItems"><HiOutlineSearch/></li>
+        </ul>
+      </ContentWrapper>
+    </header>
+  );
+};
+
+export default Header;
