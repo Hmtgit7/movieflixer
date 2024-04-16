11d09fd8aa371d13122d7628204f18fed1047f31
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-04-16 21:55:03 +0530

    adding component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
-export {default as Footer} from "./footer/Footer"
-export {default as Header} from "./header/Header"
-export {default as Img} from "./lazyLoadImage/Img"
-export {default as ContentWrapper} from "./contentWrapper/ContentWrapper"
+export { default as Footer } from "./footer/Footer";
+export { default as Header } from "./header/Header";
+export { default as Img } from "./lazyLoadImage/Img";
+export { default as ContentWrapper } from "./contentWrapper/ContentWrapper";
+export { default as SwitchTabs } from "./switchTabs/SwitchTabs";
+export { default as Carousel } from "./carousel/Carousel";
