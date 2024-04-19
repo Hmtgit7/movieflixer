0fa4d7b7db84ac0ebf71ba605978b5baeea400b0
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-04-19 18:03:04 +0530

    adding trending and popular

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,16 @@ import React from "react";
 import "./Home.scss";
 
 import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
+      <Trending/>
+      <Popular/>
+    
     </div>
   );
 };
