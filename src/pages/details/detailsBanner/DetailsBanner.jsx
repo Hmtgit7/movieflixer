2f26f252a44a040a60dedc71aa940ec1772c5df2
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-04-22 19:07:02 +0530

    deatail banner component

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -7,44 +7,95 @@ import "./DetailsBanner.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import useFetch from "../../../Hooks/useFetch.jsx";
-import Genres from "../../../components/Genres/Genres.jsx";
+import Genres from "../../../components/Genres/Genres";
 import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/lazyLoadImage/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
+import { Playbtn } from "../Playbtn.jsx";
 
 const DetailsBanner = ({ video, crew }) => {
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}`);
+  console.log(data);
 
-    const {mediaType,id}=useParams();
-    const {data,loading}=useFetch(`/${mediaType}/${id}`);
+  const { url } = useSelector((state) => state.home);
 
-    const toHoursAndMinutes = (totalMinutes) => {
-        const hours = Math.floor(totalMinutes / 60);
-        const minutes = totalMinutes % 60;
-        return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
-    };
+  const _genres = data?.genres.map((g) => g.id);
 
-    return (
-        <div className="detailsBanner">
-            {!loading ? (
-                <div>Details Content...</div>
-            ) : (
-                <div className="detailsBannerSkeleton">
-                    <ContentWrapper>
-                        <div className="left skeleton"></div>
-                        <div className="right">
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                            <div className="row skeleton"></div>
-                        </div>
-                    </ContentWrapper>
+  const toHoursAndMinutes = (totalMinutes) => {
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
+  };
+
+  return (
+    <div className="detailsBanner">
+      {!loading ? (
+        <>
+          {!!data && (
+            <React.Fragment>
+              <div className="backdrop-img">
+                <Img src={url.backdrop + data.backdrop_path} />
+              </div>
+              <div className="opacity-layer"></div>
+              <ContentWrapper>
+                <div className="content">
+                  <div className="left">
+                    {data.poster_path ? (
+                      <Img
+                        className="posterImg"
+                        src={url.backdrop + data.poster_path}
+                      />
+                    ) : (
+                      <Img className="posterImg" src={PosterFallback} />
+                    )}
+                  </div>
+                  <div className="right">
+                    <div className="title">
+                      {`${data.name || data.title} (${dayjs(
+                        data?.release_date
+                      ).format("YYYY")})`}
+                    </div>
+                    <div className="subtitle">{data.tagline}</div>
+
+                    <Genres data={_genres} />
+
+                    <div className="row">
+                      <CircleRating rating={data.vote_average.toFixed(1)} />
+                      {/* <div className="voteCount">{data.vote_count} votes</div> */}
+                      <div className="playbtn" onClick={() => {}}>
+                        <Playbtn />
+                        <span className="text">Watch Trailer</span>
+                      </div>
+                    </div>
+                    <div className="overview">
+                      <div className="heading">Overview</div>
+                      <div className="description">{data?.overview}</div>
+                    </div>
+                  </div>
                 </div>
-            )}
+              </ContentWrapper>
+            </React.Fragment>
+          )}
+        </>
+      ) : (
+        <div className="detailsBannerSkeleton">
+          <ContentWrapper>
+            <div className="left skeleton"></div>
+            <div className="right">
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+            </div>
+          </ContentWrapper>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default DetailsBanner;
