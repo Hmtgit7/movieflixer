0cb373ffc729c24c69ea98241f32950f0b84b464
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-04-22 19:07:12 +0530

    deatails page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import "./Details.scss";
+
+import useFetch from "../../Hooks/useFetch";
+import { BsJustify } from "react-icons/bs";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+
 
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+  // const { data, loading } = useFetch(`/movie/movieId`);
+  // const { mediaType, Id } = useParams();
 
-export default Details
+  return <div>
+    <DetailsBanner/>
+  </div>;
+};
+
+export default Details;
